d8cc29f019deb1e170e7ddd8b1cbf349653fed68
Yousuf Borno <yousuf.grad.26@gmail.com>
2026-06-04 00:59:26 -0700

    Day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,18 @@
+#what is time complexity
+# time taken to run the code x NOT CORRECT
+#rate of increase in time with respect to input size
+
+
+for i in range(1,6):
+    print("yousuf borno");
+    
+
+# we have to start thinking from the worst case, if it can handle worst case,
+#then its a good code
+
+
+#Space complexity(memory space)
+#Auxilary space(space used to solve the problem) + input space
+
+
+#Donot change the input, always create new variables
